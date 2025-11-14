4fd185e9c99938ef79e4d3caa3a39793ab8536f4
Jakub Jurczyk <pandetthe@gmail.com>
2025-11-14 09:45:48 +0100

    Remove stupid validation of password for signin

--- api/src/Application/Auth/SignIn/SignInCommandValidator.cs
+++ api/src/Application/Auth/SignIn/SignInCommandValidator.cs
@@ -14,20 +14,5 @@ internal sealed class SignInCommandValidator : AbstractValidator<SignInCommand>
             .WithMessage("Email must be a valid email address.")
             .MaximumLength(UserOptions.MaxEmailLength)
             .WithMessage($"Email must not exceed {UserOptions.MaxEmailLength} characters.");
-
-        RuleFor(x => x.Password)
-            .NotEmpty().WithMessage("Password is required.")
-            .MinimumLength(UserOptions.MinPasswordLength)
-            .WithMessage($"Password must be at least {UserOptions.MinPasswordLength} characters long.")
-            .MaximumLength(UserOptions.MaxPasswordLength)
-            .WithMessage($"Password must not exceed {UserOptions.MaxPasswordLength} characters.")
-            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
-            .When(x => UserOptions.RequireLowercaseInPassword)
-            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
-            .When(x => UserOptions.RequireUppercaseInPassword)
-            .Matches(@"\d").WithMessage("Password must contain at least one number.")
-            .When(x => UserOptions.RequireDigitInPassword)
-            .Matches(@"[^\da-zA-Z]").WithMessage("Password must contain at least one symbol.")
-            .When(x => UserOptions.RequireNonAlphanumeric);
     }
 }
